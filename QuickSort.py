6069a7550d79a7eff1259ca58ed09bd940129c25
Nivisha23 <66494628+Nivisha23@users.noreply.github.com>
2021-03-26 18:57:59 +0530

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -1,36 +1,31 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 26 04:36:21 2021
-
-@author: Nivisha
-"""
-
 arr = [34, 23, 76, 56, 98, 34, 20, 10, 36]
 print(arr)
 
 def quick_sort(arr):
     
-    less, greater = [], []
+    less, equal, greater = [], [], []
 
     if len(arr) <= 1:
         return arr
 
     else:
         for i in arr:
-            if i >= arr[-1]:
+            
+            if i > arr[-1]:
                 greater.append(i)
+            
+            elif i == arr[-1]:
+                equal.append(i)
+                
             else:
                 less.append(i)
-                break
-        return [quick_sort(less), quick_sort(greater)]
-    
+           
+            # print('less = ',less, 'equal = ',equal, 'greater = ', greater)
         
-            
+        return quick_sort(less)+ equal+ quick_sort(greater)
+    
 
-def some():
+def main():
     print(quick_sort(arr))
 
-
-
-some()
+main()
